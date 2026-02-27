081666ed9f22e96ebe50d45e4010db2ddceccba3
kakaxi666 <an456789123@163.com>
2026-02-27 13:53:18 +0800

    测试关于页面

--- src/page/About.vue
+++ src/page/About.vue
@@ -2,7 +2,13 @@
   <div class="about">
     <h1>关于页面</h1>
     <p>这是关于页面的内容</p>
-    <div>12312312</div>
+    <div class="myDiv">12312312</div>
+    <!-- 新增测试表单 -->
+    <el-form class="form">
+      <el-form-item>
+        <el-input placeholder="请输入内容"></el-input>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
@@ -13,6 +19,16 @@ export default {
 </script>
 
 <style scoped>
+.form {
+  margin: 20px;
+  width: 500px; 
+}
+/* 
+   新增form上面的div背景颜色天蓝色
+*/
+.myDiv{
+  background-color: aqua;
+}
 .about {
   padding: 20px;
   text-align: center;
